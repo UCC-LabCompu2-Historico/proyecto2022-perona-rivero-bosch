b11484edd2c717db74e72697aafa0067a8ccd1be
JosefinaRivero <josefinarivero03@gmail.com>
2022-06-12 21:53:11 -0300

    2

---
+++ funciones.js
@@ -0,0 +1,11 @@
+
+/**
+ * Conversion de Unidades, de metros, yardas, pies y pulgadas
+ * @method cambiarUnidades
+ * @param {string} id - producto
+ * @param {number} valor - precio
+ * @return
+ */
+function add(producto, precio){
+    console.log(producto, precio);
+}
